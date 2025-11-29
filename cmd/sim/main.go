31cf3067bbb01dc7e9bfe69a011434829943f869
Ashish Bhusal <ashish.bhusal.16999@gmail.com>
2025-11-28 19:58:56 -0800

    chore: initial scaffold and module setup

---
+++ cmd/sim/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("shard-router-study: simulator placeholder")
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bhusalashish/consistent-hashing-bounded-loads.git
+
+go 1.25.4
